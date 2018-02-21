4d4d044820ac6071c30a10cbe2ef6775d2119337
Alison Wood <alisonwood29@gmail.com>
2018-02-21 20:00:14 +0000

    populated list with beer names

--- public/app.js
+++ public/app.js
@@ -1,5 +1,37 @@
 const app = function () {
+  const url = "https://api.punkapi.com/v2/beers";
+  makeRequest(url, requestComplete);
 
 };
+
+const makeRequest = function (url, callback) {
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener("load", callback);
+  request.send();
+}
+
+const requestComplete = function () {
+  if(this.status !== 200) return;
+  const jsonString = this.responseText;
+  const beers = JSON.parse(jsonString);
+  populateList(beers);
+}
+
+
+const addListItem = function (text) {
+  const li = document.createElement('li');
+  li.innerText = text;
+  const ul = document.getElementById('beer-list');
+  ul.appendChild(li);
+}
+
+
+const populateList = function (beers) {
+  console.log(beers);
+  beers.forEach(function (beer) {
+    addListItem(beer.name);
+  })
+}
 
 document.addEventListener('DOMContentLoaded', app);
